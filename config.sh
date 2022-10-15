1c42eba92ca2737f38af428d476a89b84712942a
Thanos Kastoras <thanoskastoras@gmail.com>
2022-10-15 11:57:41 +0300

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,10 @@
+# Install basic programming tools
+sudo apt install build-essentials valgrind git python3-dev ssh vim tree neofetch fortune cowsay
+echo 'fortune -n 200 -s | cowsay -pn' >> ~/.bashrc
+
+# For Visual Studio Code:
+# Goto https://code.visualstudio.com/download
+# and download the deb package. Then run:
+sudo dpkg -i ~/Downloads/<filename>.deb
+
+
