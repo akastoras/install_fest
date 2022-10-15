2c5d58bf3b8f72c32e2b3ff1eec9ee487afd3b46
Thanos Kastoras <thanoskastoras@gmail.com>
2022-10-15 12:12:27 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,8 @@
 # Install basic programming tools
-sudo apt install build-essentials valgrind git python3-dev ssh vim tree neofetch # fortune cowsay
+sudo apt install build-essential valgrind git python3-dev ssh vim tree neofetch
+
+# For fun
+sudo apt install fortune cowsay
 echo 'fortune -n 200 -s | cowsay -pn' >> ~/.bashrc
 
 # For Visual Studio Code:
